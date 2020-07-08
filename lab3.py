b656dc0cc887a9eeb2b7132ac2ed4c13e757588a
hakan <hakandikicia@gmail.com>
2020-07-08 13:21:43 +0300

    Lab3 modified. It is working well.

--- lab3.py
+++ lab3.py
@@ -8,7 +8,7 @@ x = 0
 a = 0
 while i < len(sentence)-1:
    
-    while x < len(sentence)-1 and sentence[x] != " ":
+    while x < len(sentence) and sentence[x] != " ":
         x += 1
     words.append(sentence[a:x])
     i = x
