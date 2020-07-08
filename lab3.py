50372b2c6c3f71a95f78c2b9ec85fc224f432953
hakan <hakandikicia@gmail.com>
2020-07-08 13:12:54 +0300

    lab3 is modified and it is working.

--- lab3.py
+++ lab3.py
@@ -3,15 +3,15 @@
 #find the longest word of sentence entered by user
 sentence = input("please a sentence\n")
 i = 0
-words=[]
+words=[] #creating for store word of the sentence
 x = 0
 a = 0
-while i<len(sentence)-1:
+while i < len(sentence)-1:
    
-    while x<len(sentence)-1 and sentence[x]!=" ":
-        x+=1
+    while x < len(sentence)-1 and sentence[x] != " ":
+        x += 1
     words.append(sentence[a:x])
-    i=x
-    a=i+1
+    i = x
+    a = x = i+1
 
 print(max(words, key = len))
